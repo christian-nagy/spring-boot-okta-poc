340d7aaaa1f12e34c2f633deb49a5a927a149d22
christian-nagy <chris@21stclub.com>
2017-12-12 10:35:25 +0200

    Enable security for pokta

--- okta-poc/src/main/java/com/example/pokta/config/SecurityConfiguration.java
+++ okta-poc/src/main/java/com/example/pokta/config/SecurityConfiguration.java
@@ -1,27 +1,14 @@
 package com.example.pokta.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import static java.util.Collections.singletonList;
 
 @Configuration
-//@EnableWebSecurity
-public class SecurityConfiguration {//extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 //    @Bean
 //    UserDetailsService user() {
@@ -46,8 +33,8 @@ public class SecurityConfiguration {//extends WebSecurityConfigurerAdapter {
 //        return bean;
 //    }
 
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/health");
-//    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/health");
+    }
 }
